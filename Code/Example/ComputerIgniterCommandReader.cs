43d9ae1ae436a2c066f15803af8e825f0df93efa
Francisco Crespo <fran_cg@outlook.com>
2020-11-17 02:23:34 +0100

    Added process launcher

--- Code/Example/ComputerIgniterCommandReader.cs
+++ Code/Example/ComputerIgniterCommandReader.cs
@@ -1,5 +1,7 @@
 ﻿using System.Text;
+using System.Diagnostics;
 using System.Threading.Tasks;
+using System;
 using Microsoft.Azure.ServiceBus;
 using Microsoft.Azure.ServiceBus.Core;
 using Microsoft.Extensions.Logging;
@@ -8,11 +10,41 @@ using AlexaCommandReader;
 
 namespace AlexaCommandReaderExample {
     public class ComputerIgniterCommanBehavior : ICommandBehavior {
+        private const string computerOn = "computerOn";
+        private const string computerOff = "computerOff";
+        private const string prepareForWork = "prepareForWork";
+
+        private const string shell = "Shell";
+        private const string startupCommand = "StartupCommand";
+        private const string shutdownCommand = "ShutdownCommand";
+
         public async Task MessageBehavior(Message message, MessageReceiver messageReceiver, ILogger logger) {
             var parsedMessage = JsonConvert.DeserializeObject<AlexaMessageDTO>(Encoding.UTF8.GetString(message.Body));
             logger.LogInformation($"Received message: {parsedMessage.Skill} - {parsedMessage.Intent}");
-            logger.LogInformation($"----------------------------------------------------------------");
+
+            switch (parsedMessage.Intent) {
+                case computerOn:
+                    LaunchScript(Environment.GetEnvironmentVariable(shell), Environment.GetEnvironmentVariable(startupCommand));
+                    break;
+                case computerOff:
+                    LaunchScript(Environment.GetEnvironmentVariable(shell), Environment.GetEnvironmentVariable(shutdownCommand));
+                    break;
+                case prepareForWork:
+                    break;
+            }
+
             await messageReceiver.CompleteAsync(message.SystemProperties.LockToken);
+        }
+
+        private void LaunchScript(string executable, string args) {
+            Process proc = new Process() { 
+                StartInfo = new ProcessStartInfo() { 
+                    FileName = executable,
+                    Arguments = args
+                } 
+            };
+            proc.Start();
         }
     }
 }
+ 
