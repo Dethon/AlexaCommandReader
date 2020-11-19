20af5699654649b03cc2825d5d1ba6fb34222158
Francisco Crespo <fran_cg@outlook.com>
2020-11-19 14:20:00 +0100

    Added Windows support

--- Code/Example/Program.cs
+++ Code/Example/Program.cs
@@ -6,11 +6,11 @@ using System.Runtime.InteropServices;
 namespace AlexaCommandReaderExample {
     public class Program {
         static async Task Main() {
-            if (System.Runtime.InteropServices.RuntimeInformation.IsOSPlatform(OSPlatform.Linux)) {
-                await HostBuilderFactory.GetHostBuilder<ComputerIgniterCommandBehavior>().RunConsoleAsync();
+            if (RuntimeInformation.IsOSPlatform(OSPlatform.Linux)) {
+                await HostBuilderFactory.GetHostBuilder<ComputerIgniterCommandBehavior>().Build().RunAsync();
             }
-            else if(System.Runtime.InteropServices.RuntimeInformation.IsOSPlatform(OSPlatform.Windows)) {
-                await HostBuilderFactory.GetHostBuilder<ComputerPreparerCommandBehavior>().RunConsoleAsync();
+            else if(RuntimeInformation.IsOSPlatform(OSPlatform.Windows)) {
+                await HostBuilderFactory.GetHostBuilder<ComputerPreparerCommandBehavior>().Build().RunAsync();
             }
         }
     }
